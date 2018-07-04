0819ac743e481f1c01fdc2a3cc8bad44743a430b
Alex Gladd <alex@alexgladd.com>
2018-07-03 21:14:05 -0400

    First deploy test

---
+++ cicd/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ev
+
+BUILD_DIR=build
+BUCKET=gladdbiz-bmactivity
+
+# build scripts must have succeeded
+if [! "${TRAVIS_TEST_RESULT}" -eq "0" ] || ( exit 1 )
+
+# build must exist
+if [! -d ${BUILD_DIR} ] || ( exit 1 )
+
+# sync build with S3 bucket
+aws s3 sync ${BUILD_DIR} s3://${BUCKET} --delete
