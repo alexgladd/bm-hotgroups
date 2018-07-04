0c851b1ae79f46c3d2ac9e406a7b55d6b6ffff9f
Alex Gladd <alex@alexgladd.com>
2018-07-03 21:53:47 -0400

    Add cdn invalidation step to deploy

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -3,6 +3,7 @@ set -ev
 
 BUILD_DIR=build
 BUCKET=gladdbiz-bmactivity
+CDN_DISTRO_ID=ENWIL8ISNZ386
 
 # build scripts must have succeeded
 [[ "${TRAVIS_TEST_RESULT}" -eq "0" ]] || ( exit 1 )
@@ -12,3 +13,6 @@ BUCKET=gladdbiz-bmactivity
 
 # sync build with S3 bucket
 aws s3 sync ${BUILD_DIR} s3://${BUCKET} --delete
+
+# flush cloudfront cache
+aws cloudfront create-invalidation --distribution-id ${CDN_DISTRO_ID} --paths "/*"
