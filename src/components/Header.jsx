edd5e024fc082e64bada30c933e51f0330c4d621
Alex Gladd <alex@alexgladd.com>
2021-05-07 21:41:38 +0200

    Initial working nav menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
 import MenuButton from './MenuButton';
 
+function NavMenu({ isOpen }) {
+  return (
+    <nav id="NavMenu" role="navigation" className={isOpen ? 'Open' : undefined}>
+      <div>About</div>
+      <div>News</div>
+    </nav>
+  );
+}
+
 const propTypes = {};
 
 const defaultProps = {};
 
 export default function Header() {
+  const [ menuOpen, setMenuOpen ] = useState(false);
+
   return (
     <header id="Header">
       <h1>
         <FontAwesomeIcon icon={faBroadcastTower} />
         <span className="Title">Brandmeister Top Activity</span>
       </h1>
-      <MenuButton />
+      <MenuButton onClick={() => setMenuOpen(!menuOpen)} />
+      <NavMenu isOpen={menuOpen} />
     </header>
   );
 }
