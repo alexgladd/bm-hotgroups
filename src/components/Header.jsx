87c7f966d3b5454996581fb63328ec72157942a2
Alex Gladd <alex@alexgladd.com>
2021-05-07 20:58:35 +0200

    Add mobile menu button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
+import MenuButton from './MenuButton';
 
 const propTypes = {};
 
@@ -15,6 +16,7 @@ export default function Header() {
         <FontAwesomeIcon icon={faBroadcastTower} />
         <span className="Title">Brandmeister Top Activity</span>
       </h1>
+      <MenuButton />
     </header>
   );
 }
--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import burgerSvg from './burger-menu.svg';
 import './MenuButton.css';
 
+
 const propTypes = {
-  menuOpen: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
 const defaultProps = {};
 
-export default function MenuButton({ menuOpen, onClick }) {
+export default function MenuButton({ onClick }) {
   return (
-    <div className="MobileMenu">
-
-    </div>
+    <button id="MenuBtn" onClick={onClick}>
+      <img src={burgerSvg} alt="Hamburger menu icon" />
+    </button>
   );
 }
 
