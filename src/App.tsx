e2c07898f0d787d77f25d26ded85d39c2e836941
Alex Gladd <alex@alexgladd.com>
2024-02-05 23:51:35 -0500

    Some initial testing in v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useRef, useState } from "react";
+import BrandmeisterLastHeard from "@/lib/bmlh";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [connected, setConnected] = useState(false);
+  const bmlh = useRef<BrandmeisterLastHeard | null>(null);
+
+  if (bmlh.current === null) {
+    bmlh.current = new BrandmeisterLastHeard();
+  }
+
+  const msgFilter = (msg: object) => {
+    if (msg.DestinationID === 91 && msg.Event === "Session-Start") {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const msgHandler = (msg: object) => console.log(msg);
+
+  const onBtnClick = () => {
+    if (bmlh.current!.isConnected) {
+      bmlh.current!.close();
+    } else {
+      bmlh.current!.open();
+    }
+  };
+
+  useEffect(() => {
+    bmlh.current!.onConnectionChange((connected) => setConnected(connected));
+    bmlh.current!.onMsg(msgHandler, msgFilter);
+    // bmlh.current!.open();
+
+    return () => {
+      bmlh.current!.dropListeners();
+      bmlh.current!.close();
+    };
+  }, []);
+
+  return (
+    <main className="p-6">
+      <h1 className="py-4 text-2xl">Brandmeister Top Activity</h1>
+      <button
+        type="button"
+        onClick={onBtnClick}
+        className="p-2 bg-primary-600 text-white font-bold"
+      >
+        {connected ? "Disconnect" : "Connect"}
+      </button>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/lib/bmlh.ts
@@ -0,0 +1,142 @@
+import { io, type Socket } from "socket.io-client";
+
+const BM_DEFAULT_URL = "https://api.brandmeister.network";
+const BM_DEFAULT_OPTS = {
+  autoConnect: false,
+  path: "/lh",
+  transports: ["websocket", "polling"],
+  secure: true,
+  reconnection: true,
+};
+
+export type FnConnectionChange = (connected: boolean) => void;
+export type FnMsgFilter = (msg: object) => boolean;
+export type FnMsgHandler = (msg: object) => void;
+export type LhMsg = {
+  topic: "LH-Startup" | "LH";
+  payload: string;
+};
+
+class BrandmeisterLastHeard {
+  private connected = false;
+  private handleConnectChange: FnConnectionChange = () => {};
+  private url = BM_DEFAULT_URL;
+  private options = BM_DEFAULT_OPTS;
+  private socket: Socket;
+
+  constructor() {
+    this.socket = io(this.url, this.options);
+
+    // default event handlers
+    this.socket.on("connect", this._onConnect);
+    this.socket.on("connect_error", this._onConnectError);
+    this.socket.on("connect_timeout", this._onConnectTimeout);
+    this.socket.on("disconnect", this._onDisconnect);
+    this.socket.on("reconnect", this._onReconnect);
+    this.socket.on("reconnect_attempt", this._onReconnecting);
+    this.socket.on("reconnect_error", this._onConnectError);
+    this.socket.on("reconnect_failed", this._onReconnectFailed);
+    this.socket.on("error", this._onError);
+
+    // this.socket.on("mqtt", this._onMqtt);
+  }
+
+  get isConnected() {
+    return this.connected;
+  }
+
+  open(this: BrandmeisterLastHeard) {
+    this.socket.open();
+  }
+
+  close(this: BrandmeisterLastHeard) {
+    this.socket.close();
+  }
+
+  onConnectionChange(
+    this: BrandmeisterLastHeard,
+    handler: FnConnectionChange | null | undefined
+  ) {
+    if (handler) {
+      this.handleConnectChange = handler;
+    } else {
+      this.handleConnectChange = () => {};
+    }
+  }
+
+  onMsg(
+    this: BrandmeisterLastHeard,
+    handler: FnMsgHandler,
+    filter?: FnMsgFilter,
+    replace = true
+  ) {
+    if (replace) {
+      this.socket.off("mqtt");
+    }
+
+    this.socket.on("mqtt", (msg: LhMsg) => {
+      const lhMsg = JSON.parse(msg.payload);
+      if (!filter || filter(lhMsg)) handler(lhMsg);
+    });
+  }
+
+  dropListeners(this: BrandmeisterLastHeard) {
+    this.socket.off("mqtt");
+    this.handleConnectChange = () => {};
+  }
+
+  _onMqtt = (msg: LhMsg) => {
+    const p = JSON.parse(msg.payload);
+    console.log(`[${msg.topic}]:`, p);
+  };
+
+  _onConnect = () => {
+    console.log(`[BMLH] successfully connected to ${this.url}`);
+    this.connected = true;
+    this.handleConnectChange(this.connected);
+  };
+
+  _onConnectError = (err: Error) => {
+    console.error(`[BMLH] unable to (re)connect to ${this.url}`, err);
+    this.connected = false;
+    this.handleConnectChange(this.connected);
+  };
+
+  _onConnectTimeout = () => {
+    console.error(`[BMLH] connection timeout to ${this.url}`);
+    this.connected = false;
+    this.handleConnectChange(this.connected);
+  };
+
+  _onDisconnect = (reason: Socket.DisconnectReason) => {
+    console.log(`[BMLH] disconnected from ${this.url}: ${reason}`);
+    this.connected = false;
+    this.handleConnectChange(this.connected);
+  };
+
+  _onReconnect = () => {
+    console.log(`[BMLH] successfully reconnected to ${this.url}`);
+    this.connected = true;
+    this.handleConnectChange(this.connected);
+  };
+
+  _onReconnecting = (attempt: number) => {
+    console.log(
+      `[BMLH] attempting to reconnect to ${this.url} (attempt ${attempt})`
+    );
+    this.connected = false;
+    this.handleConnectChange(this.connected);
+  };
+
+  _onReconnectFailed = () => {
+    console.error(`[BMLH] failed to reconnect to ${this.url}`);
+    this.connected = false;
+    this.handleConnectChange(this.connected);
+  };
+
+  _onError(err: Error) {
+    console.error(`[BMLH] socket error on ${this.url}`, err);
+  }
+}
+
+export default BrandmeisterLastHeard;
